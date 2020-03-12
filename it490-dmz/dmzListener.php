9a081a65b965ac43c71da2a8fcb78c3c68ea7431
arp97-njit <60405995+arp97-njit@users.noreply.github.com>
2020-03-12 15:15:53 -0400

    file structure update

---
+++ it490-dmz/dmzListener.php
@@ -0,0 +1,83 @@
+<?php
+
+    require_once('/home/aadarsh/Desktop/git/rabbitmqphp_example/path.inc');
+    require_once('/home/aadarsh/Desktop/git/rabbitmqphp_example/get_host_info.inc');
+    require_once('/home/aadarsh/Desktop/git/rabbitmqphp_example/rabbitMQLib.inc');
+    include '/home/aadarsh/Desktop/pokeApi.php';
+
+
+
+    function requestProcessor($request){
+        echo "Recieved Request".PHP_EOL;
+        echo $request['type'];
+        var_dump($request);
+
+        if (!isset($request['type'])){
+            return array('message'=>'Error: Message type not set');
+        }
+
+        if($request['type'] == "Search") //search for pokemon
+        {
+            if(($request['pokemonNum'] == "na") && ($request['name'] == "na"))
+            { //if true - check type
+                if($request['pokeType'] == "na")
+                {  //if true - check ability
+                    if($request['ability'] == "na")
+                    {
+                        //something is wrong, shouldn't get this far
+                        //may throw an error here?
+                        $rspMessage = "No Search Param - Error";
+                    }
+                    else
+                    {
+                        //api call with ability
+                        $rspMessage = searchAbility($request['ability']);
+                    }
+                }
+                else
+                {
+                    //api call with type
+                    $rspMessage = searchType($request['pokeType']);
+                }
+            }
+            else
+            {
+                //api call with name/pokedex number
+                if($request['pokemonNum'] != "na"){
+                    $rspMessage = searchdexName($request['pokemonNum']);
+                }
+                else{
+                    $rspMessage = searchdexName($request['name']);
+                }
+            }
+        }
+        elseif($request['type'] == "Battle"){  //battle system
+            //info to send to function
+            $user1 = $request['username_1'];
+            $user2 = $request['username_2'];
+            $rspMessage = battleSystem($user1, $user2);
+        }
+        
+        elseif($request['type'] == "test"){
+        $rspMessage = "in test";
+    }
+
+    //var_dump($rspMessage);
+        return $rspMessage;
+
+    }
+
+
+    $server = new rabbitMQServer('/home/aadarsh/Desktop/git/it490-dmz/rabbitMQ_db.ini', 'testServer');
+
+    
+
+    $server->process_requests('requestProcessor');
+
+//    $returnedJson = json_decode(testCall());
+//    $exampleAb = $returnedJson -> ab;
+//    $exampleN = $returnedJson -> n;
+    //this works ^ calls the function in pokeApi.php
+
+?>
+
