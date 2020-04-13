dabff05a8ee48d8e36e7fdbce22459f76ab4f684
bryandbronstein <bryandbronstein@gmail.com>
2020-04-13 14:01:09 -0400

    added favicon to all HTML pages

--- Frontend/pokemon.php
+++ Frontend/pokemon.php
@@ -10,6 +10,7 @@ $_SESSION['pokeName'] = $_GET['name'];
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="icon" href="pokeball.png"/>
     <title>Search Our Database</title>
 </head>
 <body onload="singlePokeSearch()">
