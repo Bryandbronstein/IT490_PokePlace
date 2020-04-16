8248f90208c41924fb3fc5d76f86d9e8cb2188a9
bryandbronstein <bryandbronstein@gmail.com>
2020-04-16 11:38:11 -0400

    Fixed friendslist table

--- Frontend/functions.php
+++ Frontend/functions.php
@@ -302,20 +302,21 @@ switch ($type) {
 
         $friendsList = json_decode($friendsList_json);
 
-        $friendsTable = '<table id="friendslist_table" class="table table-hover table-dark">';
-        $friendsTable = '<thead>';
-        $friendsTable = '<tr>';
-        $friendsTable = '<th colspan="6" id="friendslist_header"><span class="tableTitle">Friends List</span></th>';
-        $friendsTable = '</tr>';
-        $friendsTable = '</thead>';
-        $friendsTable = '<tbody>';
+        $friendsTable = "";
+        $friendsTable .= '<table id="friendslist_table" class="table table-hover table-dark">';
+        $friendsTable .= '<thead>';
+        $friendsTable .= '<tr>';
+        $friendsTable .= '<th colspan="6" id="friendslist_header"><span class="tableTitle">Friends List</span></th>';
+        $friendsTable .= '</tr>';
+        $friendsTable .= '</thead>';
+        $friendsTable .= '<tbody>';
     foreach ($friendsList as $name) {
-        $friendsTable = '<tr>';
-        $friendsTable = '<td>' .$name. '</td>';
-        $friendsTable = '</tr>';
+        $friendsTable .= '<tr>';
+        $friendsTable .= '<td>' .$name. '</td>';
+        $friendsTable .= '</tr>';
     }
-        $friendsTable = '</tbody>';
-        $friendsTable = '</table>';
+        $friendsTable .= '</tbody>';
+        $friendsTable .= '</table>';
 
         echo $friendsTable;
 
