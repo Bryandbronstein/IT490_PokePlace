2d0f9b4e0ba68727608156c8f8ffbeb2fb4fc979
bryandbronstein <bryandbronstein@gmail.com>
2020-05-07 12:40:40 -0400

    fixed bundlingScript.php

--- bundlingScript.php
+++ bundlingScript.php
@@ -1,26 +1,24 @@
 <?php
-
-function bundleFiles(){
     //STEPS TO MAKE THIS SCRIPT WORK
     //1. create empty directories "directoryToBuild" and "directoryToSend"
     //2. Ensure paths to these folders are correct
     //3. any time you see frontend, replace it with the name of your machine (backend, dmz, etc.)
 
-    $a = new PharData('../directoryToBuild/frontendBundle.tar');
-    $a->buildFromDirectory('../directoryToBuild');
+    $a = new PharData('./directoryToBuild/frontendBundle.tar');
+    $a->buildFromDirectory('./directoryToBuild');
     $a->compress(Phar::GZ);
-    rename('../directoryToBuild/frontendBundle.tar.gz', '../directoryToSend/frontendBundle.tar.gz');
+    rename('./directoryToBuild/frontendBundle.tar.gz', './directoryToSend/frontendBundle.tar.gz');
 
-    $files = glob('../directoryToBuild/*');
+    $files = glob('./directoryToBuild/*');
     foreach($files as $file){
         if(is_file($file))
             unlink($file);
     }
 
-    $output = shell_exec("sudo sshpass -p 'password' scp ../directoryToSend/frontendBundle.tar.gz ubuntu@10.0.0.201:/home/ubuntu/");
+    //$output = shell_exec("sudo sshpass -p 'password' scp ../directoryToSend/frontendBundle.tar.gz ubuntu@10.0.0.201:/home/ubuntu/");
 
     //var dumps for error checking
-    $files = scandir('../directoryToSend');
+    $files = scandir('./directoryToSend');
     echo "Var dump of _directoryToSend_ contents: ";
     var_dump($files);
     echo "Var dump of _scp command_ output: ";
@@ -41,5 +39,3 @@ function bundleFiles(){
 
         return $response;
     }
-
-}
